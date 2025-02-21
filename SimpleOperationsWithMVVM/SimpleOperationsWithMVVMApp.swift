1e343605d1ba10fc6b9124a4f4587582eed4f6df
Neil Shah <nshah@lcs.on.ca>
2025-02-21 12:12:40 -0500

    Initial Commit

---
+++ SimpleOperationsWithMVVM/SimpleOperationsWithMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleOperationsWithMVVMApp.swift
+//  SimpleOperationsWithMVVM
+//
+//  Created by Neil Shah on 2025-02-21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleOperationsWithMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
